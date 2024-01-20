7dfc902085e206e5a0cc0a4adeea4762f2e9bb1a
Rafiul29 <rafiulislam4472@gmail.com>
2024-01-20 21:04:36 +0600

    static added

--- src/components/Animated/Number.js
+++ src/components/Animated/Number.js
@@ -2,20 +2,14 @@ import React from 'react';
 import { useSpring, animated } from 'react-spring';
 
 const Number = ({ n }) => {
-    const springs = useSpring({
-        from: { x: 0 },
-        to: { x: 100 },
-    })
-    
     const { number } = useSpring({
         from: { number: 0 },
         number: n,
         delay: 200,
         config: { mass: 1, tension: 10, friction: 5 }
     })
-    
     return (
-        <animated.div>{springs.to((n) => n.toFixed(0))}</animated.div>
+        <animated.div>{number.to((n) => n.toFixed(0))}</animated.div>
     );
 };
 
